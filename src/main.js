bd408ad3eda011f016f5b9d04cb97f4000ddb25c
shenrui <490335922@qq.com>
2020-10-23 10:43:25 +0800

    初始化

--- src/main.js
+++ src/main.js
@@ -5,8 +5,10 @@ import store from './store'
 
 Vue.config.productionTip = false
 
+import './style/index.less'
+
 new Vue({
-  router,
-  store,
-  render: h => h(App)
+    router,
+    store,
+    render: h => h(App)
 }).$mount('#app')
